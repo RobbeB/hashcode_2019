def7a2d94fa2cc35a245c874efbfda39f994ea21
Matthias Goossens <matthias.goossens@tailorfit.eu>
2019-02-28 20:21:16 +0100

    optimal match function v1.0

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import { processHashcodeFile } from './fileService';
-import { getHorizontalPhotos, getVerticalPhotos, sortByAmountOfTags } from './photoService';
+import { getHorizontalPhotos, getVerticalPhotos, sortByAmountOfTags, loopPhotos } from './photoService';
 
 const photos = processHashcodeFile('files/input/b_lovely_landscapes.txt');
 
 const horizontal = getHorizontalPhotos(photos);
 const vertical = getVerticalPhotos(photos);
 
-sortByAmountOfTags(horizontal).forEach(photo => console.log(photo.toPrintFormat()));
+const sorted = sortByAmountOfTags(horizontal);
 
+
+const result = loopPhotos(sorted);
+
+result.forEach((item) => console.log(item.toPrintFormat()));
--- src/photoService.js
+++ src/photoService.js
@@ -11,3 +11,36 @@ export const sortByAmountOfTags = (photos) => {
     return b.amountOfTags - a.amountOfTags;
   });
 }
+
+export const loopPhotos = (photos) => {
+  const result = [];
+  const item = photos.shift();
+  result.push(item);
+  while (photos.length > 1) {
+    const optimal = getOptimalMatch(item, photos);
+    result.push(optimal);
+  }
+
+  result.push(photos[0]);
+
+  return result;
+}
+
+export const getOptimalMatch = (item, arr) => {
+  let optimalMatch = null;
+  let numberOfMatches = -1;
+
+	arr.forEach((photo) => {
+    let intersection = photo.tags.filter(x => item.tags.includes(x));
+  	const match = intersection.length;
+    if (match < photo.tags.length / 2 + 1 && match > numberOfMatches) {
+      optimalMatch = photo;
+      numberOfMatches = match;
+    }
+  })
+
+  const index = arr.findIndex(x => x.id === optimalMatch.id);
+  if (index) arr.splice(index, 1);
+
+  return optimalMatch;
+}
